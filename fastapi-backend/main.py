c19485bc1434f52cdc53b8a2f7a4745cdaccae91
Tianyi Pan <tianyi@fuzflo.com>
2025-04-09 15:38:05 +0800

    Update port to 3000

--- fastapi-backend/main.py
+++ fastapi-backend/main.py
@@ -62,4 +62,4 @@ async def get_session():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True) 
+    uvicorn.run("main:app", host="0.0.0.0", port=3000, reload=True) 
